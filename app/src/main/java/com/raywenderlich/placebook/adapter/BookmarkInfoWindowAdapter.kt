4a7a25c4f92ddb937b5af09aad04bba82d3ad6e7
dillpickled9 <dillpickled9@gmail.com>
2022-09-10 22:51:26 -0400

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/placebook/adapter/BookmarkInfoWindowAdapter.kt
@@ -0,0 +1,30 @@
+package com.raywenderlich.placebook.adapter
+
+import android.app.Activity
+import android.graphics.Bitmap
+import android.view.View
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import com.raywenderlich.placebook.databinding.ContentBookmarkInfoBinding
+import com.raywenderlich.placebook.ui.MapsActivity
+
+class BookmarkInfoWindowAdapter(context: Activity) :
+    GoogleMap.InfoWindowAdapter {
+
+    private val binding =
+        ContentBookmarkInfoBinding.inflate(context.layoutInflater)
+
+    override fun getInfoWindow(marker: Marker): View? {
+        val imageView = binding.photo
+        imageView.setImageBitmap((marker.tag as MapsActivity.PlaceInfo).image)
+        // This function is required, but can return null if
+        // not replacing the entire info window
+        return null
+    }
+
+    override fun getInfoContents(marker: Marker): View? {
+        binding.title.text = marker.title ?: ""
+        binding.phone.text = marker.snippet ?: ""
+        return binding.root
+    }
+}
